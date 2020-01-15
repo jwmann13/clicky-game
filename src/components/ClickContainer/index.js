9cd64573634028245e068afde6205baf6f734bcc
Jeffrey Mann <jwmann22@gmail.com>
2020-01-14 20:30:42 -0500

    stable pictures and a lot of other things

--- src/components/ClickContainer/index.js
+++ src/components/ClickContainer/index.js
@@ -3,6 +3,8 @@ import Clickable from "../Clickable";
 
 import API from "../../utils/API";
 
+import clicks from "../../clicks.json";
+
 class ClickContainer extends Component {
   state = {
     pictures: [],
@@ -10,53 +12,144 @@ class ClickContainer extends Component {
   };
 
   componentDidMount() {
-    this.getPictures();
+    this.setState(state => {
+      return {
+        clickables: this.makeClickables(clicks)
+      }
+    })
+  }
+
+  makeClickables(pics) {
+    const clickables = pics.map((d, i) => {
+      return (
+        <Clickable
+          key={d.id}
+          img={d.img}
+          id={d.key}
+          clicked={d.clicked}
+          increaseScore={this.props.increaseScore}
+          resetScore={this.props.resetScore}
+          setMessage={this.props.setMessage}
+          registerClick={key => this.registerClick(key)}
+          shuffleClickables={() => this.shuffleClickables()}
+        />
+      );
+    });
+    return clickables;
   }
 
   getPictures() {
-    API.search().then(response => {
-      console.log(response);
-      this.setState({ pictures: response.data });
+    API.search()
+      .then(response => {
+        return response.data;
+      })
+      .then(data => {
+        let newPictures = [];
+
+        for (let i = 0; i < data.length; i++) {
+          const picture = data[i];
+          newPictures.push({
+            key: picture.id,
+            img: picture.urls.raw,
+            clicked: false
+          });
+        }
+
+        return newPictures;
+      })
+      .then(pictures => {
+        this.setState({
+          pictures
+        });
+      })
+      .then(() => {
+        this.setState({
+          clickables: this.makeClickables()
+        });
+      });
+  }
+
+  shuffle(array) {
+    var currentIndex = array.length;
+    var temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  shuffleClickables() {
+    let shuffledClicks = this.shuffle(this.state.clickables);
+    this.setState({
+      pictures: shuffledClicks
     });
   }
 
-  shuffle() {}
+  resetClicks() {
+    this.setState(state => {
+      return {
+        pictures: state.pictures.map(pic => {
+          return {
+            key: pic.key,
+            img: pic.img,
+            clicked: false
+          };
+        })
+      };
+    });
+  }
+
+  registerClick(key) {
+    let clicked = this.state.pictures.filter(pic => pic.key === key);
+    console.log(key, clicked);
+  }
+
+  resetScore = () => {
+    this.props.resetScore();
+    this.resetClicks();
+  };
+
+  increaseScore = () => {
+    this.props.increaseScore();
+  };
 
   render() {
+    console.log(this.state);
+
     return (
       <div className="container">
         <div className="row">
-          {this.state.pictures.map((d, i) => {
-            console.log(d);
+          {this.state.clickables.map((c, i) => {
             if ((i + 1) % 5 === 0) {
-              return (
+              const row = (
                 <>
-                  <Clickable
-                    img={d.urls.raw}
-                    key={d.id}
-                    increaseScore={this.props.increaseScore}
-                    resetScore={this.props.resetScore}
-                    setMessage={this.props.setMessage}
-                  />
-                  <div className="w-100" key={i}></div>
+                  {c}
+                  <RowBreak />
                 </>
               );
+              return row;
             } else {
-              return (
-                <Clickable
-                  img={d.urls.raw}
-                  key={d.id}
-                  increaseScore={this.props.increaseScore}
-                  resetScore={this.props.resetScore}
-                  setMessage={this.props.setMessage}
-                />
-              );
+              return c;
             }
           })}
         </div>
       </div>
     );
   }
+}
+
+function RowBreak(props) {
+  return <div className="w-100" />;
 }
 
 export default ClickContainer;
--- src/components/Clickable/index.js
+++ src/components/Clickable/index.js
@@ -4,29 +4,32 @@ import "./index.css";
 class Clickable extends Component {
   state = {
     clicked: false
-  };
+  }
 
-  clickRegister = () => {
+  click = () => {
+    this.props.registerClick(this.props.id);
     if (!this.state.clicked) {
-      this.setState({ clicked: true });
+      this.setState({ clicked: true })
       this.props.increaseScore();
       this.props.setMessage("correct");
     } else {
       this.props.resetScore();
       this.props.setMessage("incorrect");
     }
+    this.props.shuffleClickables();
   };
 
   render() {
     return (
-      <>
         <button
+          key={this.props.id}
           className="btn col"
-          onClick={this.clickRegister}
+          onClick={this.click}
+          data-sclicked={this.state.clicked}
+          data-pclicked={this.props.clicked}
         >
           <img src={this.props.img} alt={this.props.alt} />
         </button>
-      </>
     );
   }
 }
